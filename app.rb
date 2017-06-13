8220aeef6476c8fbf8e7d41a9826762fb94bb089
Timothy Hsu <thsu@zendesk.com>
2017-06-13 11:13:13 -0700

    polling now working

--- app.rb
+++ app.rb
@@ -4,4 +4,14 @@ class YoutubeIntegration < Sinatra::Base
 	get '/' do
 		"Hello World!"		
 	end
+
+	get '/manifest' do
+		content_type :json
+		File.read('manifest.json.erb')
+	end
+
+	post '/pull' do
+		puts params.inspect
+		"pull page"
+	end
 end
