58b9fa952ce9427e20a12ed789167f54ffe0ddec
Timothy Hsu <thsu@zendesk.com>
2017-06-23 13:04:01 -0700

    successfully grabbing all videoIds from a channel and its corresponding comments

--- controllers/pull_controller.rb
+++ controllers/pull_controller.rb
@@ -32,29 +32,68 @@ module Controllers
 				service = Google::Apis::YoutubeV3::YouTubeService.new
 				service.authorization = auth_client
 
-				videoIds = []
 				puts metadata["credentials"]
-				response = service.list_searches('snippet', max_results: 50, for_mine: true, type: 'video')
-				.to_json
-				JSON.parse(response).fetch('items').each do |video|
-					videoIds.push(video.fetch('id').fetch('videoId'))
-				end
-				
-				nextPageToken = JSON.parse(response).fetch('nextPageToken') # Gets my page token for the next page
-				while !nextPageToken.nil? && !nextPageToken.empty? # Loops through all of my pages until there are no more pages
-					puts nextPageToken
-					response = service.list_searches('snippet', max_results: 50, for_mine: true, page_token: nextPageToken, type: 'video').to_json
-					if JSON.parse(response).key?('nextPageToken')
-						nextPageToken = JSON.parse(response).fetch('nextPageToken') # Gets my page token for the next page
-						JSON.parse(response).fetch('items').each do |video|
-							videoIds.push(video.fetch('id').fetch('videoId'))
-						end
-					else
-						nextPageToken = ""
-					end
-				end
-				puts videoIds
+
+				content = {}
+				content = PullController.grab_all_videos_and_their_comments(service, content)
+
+				# puts content
 			end
 		end
+
+		## 
+		# This method grabs all the videos from the authorized channel and will begin also grabbing all the comments from
+		# those videos
+		#
+		def self.grab_all_videos_and_their_comments(service, content)
+			response = service.list_searches('snippet', max_results: 50, for_mine: true, type: 'video')
+				.to_json
+			JSON.parse(response).fetch('items').each do |video|
+				videoId = video.fetch('id').fetch('videoId')
+				videoTitle = video.fetch('snippet').fetch('title')
+				puts "===================#{videoTitle}================="
+				comments = PullController.get_all_comments(service, videoId)
+				if comments == false
+					next
+				end
+				details = [videoTitle, comments]
+				content[videoId] = details
+			end
+
+			while JSON.parse(response).include?('nextPageToken')
+				nextPageToken = JSON.parse(response).fetch('nextPageToken') # Gets my page token for the next page
+				response = service.list_searches('snippet', max_results: 50, for_mine: true, page_token: nextPageToken, type: 'video').to_json
+				JSON.parse(response).fetch('items').each do |video|
+					videoId = video.fetch('id').fetch('videoId')
+					videoTitle = video.fetch('snippet').fetch('title')
+					puts "===================#{videoTitle}================="
+					comments = PullController.get_all_comments(service, videoId)
+					if comments == false
+						next
+					end
+					details = [videoTitle, comments]
+					content[videoId] = details
+				end
+			end
+			return content
+		end
+
+		## 
+		# This method grabs all the comments from a particular videoId.
+		# 
+		def self.get_all_comments(service, videoId)
+			begin
+				response = service.list_comment_threads('snippet,replies', video_id: videoId).to_json
+				comments = JSON.parse(response).fetch('items')
+				while JSON.parse(response).include?('nextPageToken')
+					nextPageToken = JSON.parse(response).fetch('nextPageToken') # Gets my page token for the next page
+					response = service.list_comment_threads('snippet,replies', video_id: videoId, page_token: nextPageToken).to_json
+					comments + JSON.parse(response).fetch('items')
+				end
+			rescue Exception => e # this catches the error when videos are made private thus disabling comments
+				puts e
+			end
+		end
+
 	end
 end
