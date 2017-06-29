cd57846f76609e0d5616abeafc326124a9d60c9d
Timothy Hsu <thsu@zendesk.com>
2017-06-29 16:37:32 -0700

    added Procfile

--- controllers/pull_controller.rb
+++ controllers/pull_controller.rb
@@ -51,7 +51,6 @@ module Controllers
 						if topLevelComment[:created_at] > last_pull_time
 							external_resources.push(topLevelComment)
 						end
-						puts 'OVER HERE'
 						if commentThread.include?('replies')
 							commentThread.fetch('replies').fetch('comments').reverse_each do |comment|
 								reply = PullController.create_reply(comment)
@@ -62,7 +61,6 @@ module Controllers
 						end
 					end
 				end
-				puts 'IM HERE'
 				puts external_resources
 				return response = {
 				"external_resources": external_resources,
