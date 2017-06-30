24b145cb8dc6e35e30e454b61d6674c0a5a14e5c
Timothy Hsu <thsu@zendesk.com>
2017-06-30 12:13:11 -0700

    added parent_id to response for commentThread

--- controllers/pull_controller.rb
+++ controllers/pull_controller.rb
@@ -170,6 +170,7 @@ module Controllers
 					"external_id": "#{video_id}&lc=#{comment_id}",
 					"message": "#{message}",
 					"created_at": "#{publish_date}",
+					"parent_id": "#{comment_id}",
 					"author": {
 						"external_id": "#{author_id}",
 						"name": "#{author_display_name}",
